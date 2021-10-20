41bb5810d6d4820669e6ff1465248e88498f2647
Anthony Villegas <villegasanthony87@yahoo.com>
2021-10-20 10:57:44 -0500

    feat: pt 1 of sessions and cookies

--- src/main/java/ViewProfileServlet.java
+++ src/main/java/ViewProfileServlet.java
@@ -8,6 +8,11 @@ import java.io.IOException;
 @WebServlet(name = "ViewProfileServlet", urlPatterns = "/profile")
 public class ViewProfileServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String user = (String) request.getSession().getAttribute("user");
+        if(user == null) {
+            response.sendRedirect("/login");
+            return;
+        }
         request.getRequestDispatcher("/profile.jsp").forward(request, response);
     }
 }
